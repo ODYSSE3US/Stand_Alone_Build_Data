5b4efc0480aee8b39849f3ec996df4c6b79ce9fe
ODYSSƎEUS <holocron.ltd@gmail.com>
2022-03-14 16:02:01 +0000

    git push/pull shortcut
    Unfinished.

---
+++ Developer_Update.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+#Nowhere near runnable.
+
+import os
+import sys
+
+if os.name == "nt":
+    pass
+#    sys.os("input")
+#    sys.os("input")
+#    sys.os("input")
+
+else:
+    sys.os("rm -rf Stand_Alone_Build_Data")
+    sys.os("input")
+    sys.os("input")
